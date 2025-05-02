79e5f83401a3373ee2a6300d61bad6ec0389a979
Павел Яковлев <p.yakowlev2014@yandex.ru>
2025-05-02 18:02:56 +0300

    Это пока базовая модель пинга, буду дорабатывать

---
+++ Cursach_Backend/Controllers/PingController.cs
@@ -0,0 +1,29 @@
+﻿using Cursach_Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+using System.Text.Json;
+
+namespace Cursach_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PingController : Controller
+    {
+        private static string ping = "";
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return JsonSerializer.Serialize(new PingModel() { id = 1, description = "", status = "Pinging" });
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string ping)
+        {
+            return ping + "Ping successfully posted";
+        }
+
+
+    }
+}
---
+++ Cursach_Backend/Models/PingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Cursach_Backend.Models
+{
+    public class PingModel
+    {
+        public int id {  get; set; }
+        public string description { get; set; }
+        public string status { get; set; }
+    }
+}
